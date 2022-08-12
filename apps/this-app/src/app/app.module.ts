5d3471c00e38e6c14b32c262fde532eb7f67fe36
Kathir S <kathirs@hash8.io>
2022-08-12 23:20:00 +0530

    created nav components

--- apps/this-app/src/app/app.module.ts
+++ apps/this-app/src/app/app.module.ts
@@ -4,10 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavLayoutComponent } from './components/nav-layout/nav-layout.component';
+import { NavItemComponent } from './components/nav-item/nav-item.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'app',
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    NavLayoutComponent,
+    NavItemComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
